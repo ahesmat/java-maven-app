90ded157f9d8681f387333af528902005cf7cb0b
ahesmat <ahmed.moh.esmat@gmail.com>
2023-11-14 20:14:09 -0800

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,27 +1,9 @@
-package com.example;
+public class HelloWorld {
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+    public static void main(String[] args) {
 
-@SpringBootApplication
-public class Application {
-
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
     }
 
-    @PostConstruct
-    public void init()
-    {
-        Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started");
-    }
-
-    public String getStatus() {
-        return "OK";
-    }
 }
